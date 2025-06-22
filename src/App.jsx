afecb7fab14bf285f45af2c16cb7cce2921f70cd
Omkar <ombabar9866@gmail.com>
2025-06-22 19:35:47 +0530

    SigninSignUp Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react'
+import Auth from './components/auth.jsx';
 
 function App() {
-  
+  return (
+    <div>
+      <Auth />
+    </div>
+  );
 }
 
-export default App
+export default App;
