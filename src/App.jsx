0310ea1dc55d4e6c857ba7ad7ff1811f7b77eed9
nupur <nupurshankhapal@gmail.com>
2025-06-25 16:35:35 +0530

    footer created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 // import Auth from './components/auth.jsx';
 import { NavBarComponent } from './components/navbarBeforeSignup.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <div>
       {/* <Auth /> */}
       <NavBarComponent />
+      <Footer/>
     </div>
   );
 }
