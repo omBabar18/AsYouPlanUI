f08b766ec328aa8cfe0c748b4712f079ded2e80a
Manish230203 <manishkenjale07@gmail.com>
2025-07-18 17:18:49 +0530

    Static pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import { NavBarComponent } from './components/navbarBeforeSignup.jsx';
 import HomePage from './pages/HomePage.jsx';
 import { WhyToChooseUsComponent } from './components/whyToChooseUs.jsx';
 import Footer from './components/footer.jsx';
+=======
+import Footer from './components/Footer.jsx';
+import CustomerReview from './components/CustomerReview.jsx';
 
 function App() {
   return (
     <div>
       {/* <Auth /> */}
+<<<<<<< HEAD
       {/* <NavBarComponent /> */}
       <HomePage/>
       {/* <WhyToChooseUsComponent /> */}
